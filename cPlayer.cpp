e60237f0fb5365fc24a867a887a35826338d0398
cumaskr <cumaskr@naver.com>
2017-01-10 14:16:54 +0900

    아이템 목록및 플레이어와 아이템관계로직 수정(전우형)

--- cPlayer.cpp
+++ cPlayer.cpp
@@ -6,11 +6,14 @@ HRESULT cPlayer::init(void)
 {
 	_bullet = new bullet();
 	_bullet->init("총알",50);
+
+	IsFly = false;
+
 	key = 0;
 	money = 0;
 	bomb = 0;
 	arrow = 기본총알;
-	space = 모래시계;
+	space = 없음;
 
 	x = WINSIZEX / 2;
 	y = WINSIZEY / 2;
@@ -32,33 +35,46 @@ void cPlayer::release(void)
 
 void cPlayer::update(void)
 {
-	int static nCount = 0;
-	nCount++;
-	if (nCount>1000)
+
+//==================================================================
+//		## 총알 종류별로 로직짜야함 ##
+//==================================================================
+	if (arrow==기본총알)
 	{
-		nCount = 0;
+		int static nCount = 0;
+		nCount++;
+		if (nCount>1000)
+		{
+			nCount = 0;
+		}
+		if (nCount%tears == 0)
+		{
+			if (KEYMANAGER->isStayKeyDown(VK_UP))
+			{
+				_bullet->fire(x, y, PI / 2, range, damage, bulletSpeed);
+			}
+			if (KEYMANAGER->isStayKeyDown(VK_DOWN))
+			{
+				_bullet->fire(x, y, 3 * PI / 2, range, damage, bulletSpeed);
+			}
+			if (KEYMANAGER->isStayKeyDown(VK_LEFT))
+			{
+				_bullet->fire(x, y, PI, range, damage, bulletSpeed);
+			}
+			if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
+			{
+				_bullet->fire(x, y, 0, range, damage, bulletSpeed);
+			}
+		}
 	}
-	if (nCount%tears==0)
+	if (arrow==유황)
 	{
-		if (KEYMANAGER->isStayKeyDown(VK_UP))
-		{
-			_bullet->fire(x, y, PI/2, range, damage, bulletSpeed);
-		}
-		if (KEYMANAGER->isStayKeyDown(VK_DOWN))
-		{
-			_bullet->fire(x, y, 3*PI / 2, range, damage, bulletSpeed);
-		}
-		if (KEYMANAGER->isStayKeyDown(VK_LEFT))
-		{
-			_bullet->fire(x, y, PI, range, damage, bulletSpeed);
-		}
-		if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
-		{
-			_bullet->fire(x, y, 0, range, damage, bulletSpeed);
-		}
+
 	}
 
-	
+//==================================================================
+//		## 플레이어 움직임 로직 ##
+//==================================================================
 
 	if (KEYMANAGER->isStayKeyDown('D'))
 	{
@@ -77,6 +93,9 @@ void cPlayer::update(void)
 		y += moveSpeed;
 	}
 
+//==================================================================
+//		## 폭탄사용 ##
+//==================================================================
 	if (KEYMANAGER->isOnceKeyDown('E'))
 	{
 		if (bomb>0)
@@ -85,16 +104,75 @@ void cPlayer::update(void)
 		}		
 	}
 
+//==================================================================
+//		## 액티브아이템사용 ##
+//==================================================================
+	if (KEYMANAGER->isOnceKeyDown(VK_SPACE))
+	{
+		if (space==박사의조정기)
+		{
+			_im->ItemUse(박사의조정기);
+		}
+		if (space == 모래시계)
+		{
+			_im->ItemUse(모래시계);
+		}
+	}
+
+//==================================================================
+//		## 카드아이템사용 ##
+//==================================================================
+	if (KEYMANAGER->isOnceKeyDown('Q'))
+	{
+		if (card==카드)
+		{
+			_im->ItemUse(카드);
+		}
+	}
+
+
 	_bullet->update();
 	rc = RectMakeCenter(x, y, PLAYERSIZE, PLAYERSIZE);
 }
 
 void cPlayer::render(void)
 {
+
+	for (int i = 0; i < vInventory.size(); i++)
+	{
+		if (vInventory[i]==유황)
+		{
+			//플레이어 이미지바꿈
+		}
+		if (vInventory[i]==구렁텅이의군주)
+		{
+			//플레이어 이미지바꿈
+		}
+	}
+	
+	if (arrow==기본총알)
+	{
+		if (damage==1)
+		{
+			_bullet->render();
+		}
+		if (damage==3) //카드먹으면 공2올라감
+		{
+			//총알더큰걸로 이미지 출력
+		}
+	}
+	if (arrow==유황)
+	{
+		// 총알에 이미지 바꿔서 출력
+	}
+	
+
+
+
 	TCHAR buffer[256];
 	wsprintf(buffer, TEXT("bomb:%d,  key:%d"), bomb, key);
 	TextOut(getMemDC(), 10, 10, buffer, lstrlen(buffer));
-	_bullet->render();
+
 	RectangleMake(getMemDC(), rc);
 }
 
--- cPlayer.h
+++ cPlayer.h
@@ -5,6 +5,16 @@
 
 class cItemManager;
 
+enum ESTATE
+{
+	IDLE,
+	DAMAGED,
+	// 카드애니매이션일떄 카드먹을때 아이템쪽에서 능력치올려주고 플레이어상태를 얘로바꿈 
+	// 플레이어쪽에서는 얘상태일때 약드는애니매이션? 출력하고 프레임이 최대로 가면 IDLE로 바꾸는로직짜면될듯
+	CARDANIM, 
+};
+
+
 class cPlayer : public gameNode
 {
 private:
@@ -15,8 +25,13 @@ private:
 	RECT rc;
 	float x;
 	float y;	
+	int state = IDLE;
 
-	int currentmap;
+	// 만약에 날개아이템(구렁텅잉의군주?)을 먹으면 아이템 매니져쪽에서 얘를 true로 바꿀거임.
+	// 맵매니져쪽에서 픽셀충돌을 IsFly==false일때만 검사하고 true가되면 픽셀충돌 하지않게 짜면될듯?
+	bool IsFly = false; 
+
+	vector<int> vInventory;
 
 //==================================================================
 //		## 스탯 ## 추가로들어갈거 수정필수!
@@ -36,22 +51,28 @@ private:
 	int bomb;   //폭탄
 	int arrow;  //현재총알종류
 	int space;  //아이템
-
+	int card; //능력치올려주는카드
 
 public:
 	void SetIM(cItemManager* iim) { _im = iim; }
-
-	void SetCurrentMap(int _currentmap) { currentmap = _currentmap; }
-	int GetCurrentMap() { return currentmap; }
-
-	int GetKey() { return key; }
-	void SetKey(int _key) { key = _key; }
+	
+	bool GetIsFly() { return IsFly; }
+	void SetIsFly(bool _IsFly) { IsFly = _IsFly; }
 
 	int GetMoney() { return money; }
 	void SetMoney(int _money) { money = _money; }
 
+	int GetKey() { return key; }
+	void SetKey(int _key) { key = _key; }	
+
 	int GetBomb() { return bomb; }
 	void SetBomb(int _bomb) { bomb = _bomb; }
+
+	int GetArrow() { return arrow; }
+	void SetArrow(int _arrow) { arrow = _arrow; }
+
+	int GetSpace() { return space; }
+	void SetSpace(int _space) { space = _space; }
 
 	void SetX(float _x) { x = _x; }
 	float GetX() { return x; }
