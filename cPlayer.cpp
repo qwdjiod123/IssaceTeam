80ef0d19ce9531aafa9d8cd90010fa87428ef15c
Jeon <cumaskr@naver.com>
2017-01-11 11:48:00 +0900

    플레이어 재수정!

--- cPlayer.cpp
+++ cPlayer.cpp
@@ -4,22 +4,12 @@
 
 HRESULT cPlayer::init(void)
 {
-	_bullet = new bullet();
-	_bullet->init("총알",50);
-
 	IsFly = false;
-
 	key = 0;
 	money = 0;
 	bomb = 0;
 	arrow = 기본총알;
-<<<<<<< HEAD
-	space = 혈사;
-	//플레이어 그려주는 정보
-=======
 	space = 없음;
-
->>>>>>> origin/master
 	x = WINSIZEX / 2;
 	y = WINSIZEY / 2;
 	moveSpeed = 3.0f;
@@ -29,108 +19,51 @@ HRESULT cPlayer::init(void)
 	damage = 1;
 	tears = 10;
 	bulletSpeed = 3;
-	rc = RectMakeCenter(x, y, PLAYERSIZEX,PLAYERSIZEY); //플레이어 몸통
-	rcHead = RectMakeCenter(x, y - PLAYERSIZEY / 2 - PLAYERHEADSIZEY / 2 +15, PLAYERHEADSIZEX, PLAYERHEADSIZEY);//플레이어 머리 
+	rc = RectMakeCenter(x, y, PLAYERSIZEX, PLAYERSIZEY); //플레이어 몸통
+	rcHead = RectMakeCenter(x, y - PLAYERSIZEY / 2 - PLAYERHEADSIZEY / 2 + 15, PLAYERHEADSIZEX, PLAYERHEADSIZEY);//플레이어 머리 
 	frameCount = 0;
 	leftFrame = 0;
 	rightFrame = 0;
 	upDownFrame = 0;
 	returnCount = 0;
-	fire = false; 
+	fire = false;
 	return S_OK;
 }
 
 void cPlayer::release(void)
 {
-	SAFE_DELETE(_bullet);
-	
+
+
 }
 
 void cPlayer::update(void)
 {
-<<<<<<< HEAD
-	int static nCount = 0;
-	nCount++;
-	if (nCount>1000)
-	{
-		nCount = 0;
-	}
-	if (nCount%tears == 0)
-	{
-		if (KEYMANAGER->isStayKeyDown(VK_UP))
-		{
-			if (faceState == pLEFT)
-			{
 
-			}
-			else 
+
+	if (KEYMANAGER->isStayKeyDown(VK_UP))
+	{
+		if (faceState == pLEFT)
+		{
+
+		}
+		else
 			faceState = pUP;
-			fire = true;
-			_bullet->fire(x, y, PI / 2, range, damage, bulletSpeed);
-		}
-		if (KEYMANAGER->isStayKeyDown(VK_DOWN))
-		{
-			faceState = pIDLE;
-			fire = true;
-			_bullet->fire(x, y, 3 * PI / 2, range, damage, bulletSpeed);
-		}
-		if (KEYMANAGER->isStayKeyDown(VK_LEFT))
-		{
-			faceState = pLEFT;
-			fire = true;
-			_bullet->fire(x, y, PI, range, damage, bulletSpeed);
-=======
+		fire = true;
 
-//==================================================================
-//		## 총알 종류별로 로직짜야함 ##
-//==================================================================
-	if (arrow==기본총알)
-	{
-		int static nCount = 0;
-		nCount++;
-		if (nCount>1000)
-		{
-			nCount = 0;
->>>>>>> origin/master
-		}
-		if (nCount%tears == 0)
-		{
-<<<<<<< HEAD
-			faceState = pRIGHT;
-			fire = true;
-			_bullet->fire(x, y, 0, range, damage, bulletSpeed);
-=======
-			if (KEYMANAGER->isStayKeyDown(VK_UP))
-			{
-				_bullet->fire(x, y, PI / 2, range, damage, bulletSpeed);
-			}
-			if (KEYMANAGER->isStayKeyDown(VK_DOWN))
-			{
-				_bullet->fire(x, y, 3 * PI / 2, range, damage, bulletSpeed);
-			}
-			if (KEYMANAGER->isStayKeyDown(VK_LEFT))
-			{
-				_bullet->fire(x, y, PI, range, damage, bulletSpeed);
-			}
-			if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
-			{
-				_bullet->fire(x, y, 0, range, damage, bulletSpeed);
-			}
->>>>>>> origin/master
-		}
 	}
-	if (arrow==유황)
+	if (KEYMANAGER->isStayKeyDown(VK_DOWN))
 	{
+		faceState = pIDLE;
+		fire = true;
 
-<<<<<<< HEAD
-=======
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_LEFT))
+	{
+		faceState = pLEFT;
+		fire = true;
+
 	}
 
-//==================================================================
-//		## 플레이어 움직임 로직 ##
-//==================================================================
-
->>>>>>> origin/master
 	if (KEYMANAGER->isStayKeyDown('D'))
 	{
 		faceState = pIDLE;
@@ -156,26 +89,17 @@ void cPlayer::update(void)
 		y += moveSpeed;
 	}
 
-//==================================================================
-//		## 폭탄사용 ##
-//==================================================================
 	if (KEYMANAGER->isOnceKeyDown('E'))
 	{
-		if (bomb>0)
+		if (bomb > 0)
 		{
 			_im->ItemUse(폭탄);
-		}		
+		}
 	}
-<<<<<<< HEAD
-	animation();
-=======
 
-//==================================================================
-//		## 액티브아이템사용 ##
-//==================================================================
 	if (KEYMANAGER->isOnceKeyDown(VK_SPACE))
 	{
-		if (space==박사의조정기)
+		if (space == 박사의조정기)
 		{
 			_im->ItemUse(박사의조정기);
 		}
@@ -185,22 +109,17 @@ void cPlayer::update(void)
 		}
 	}
 
-//==================================================================
-//		## 카드아이템사용 ##
-//==================================================================
 	if (KEYMANAGER->isOnceKeyDown('Q'))
 	{
-		if (card==카드)
+		if (card == 카드)
 		{
 			_im->ItemUse(카드);
 		}
 	}
 
-
->>>>>>> origin/master
-	_bullet->update();
+	animation();
 	rc = RectMakeCenter(x, y, PLAYERSIZEX, PLAYERSIZEY);
-	rcHead = RectMakeCenter(x, y - PLAYERSIZEY / 2 - PLAYERHEADSIZEY / 2+15, PLAYERHEADSIZEX, PLAYERHEADSIZEY);
+	rcHead = RectMakeCenter(x, y - PLAYERSIZEY / 2 - PLAYERHEADSIZEY / 2 + 15, PLAYERHEADSIZEX, PLAYERHEADSIZEY);
 }
 
 void cPlayer::render(void)
@@ -208,44 +127,16 @@ void cPlayer::render(void)
 
 	for (int i = 0; i < vInventory.size(); i++)
 	{
-		if (vInventory[i]==유황)
+		if (vInventory[i] == 유황)
 		{
 			//플레이어 이미지바꿈
 		}
-		if (vInventory[i]==구렁텅이의군주)
+		if (vInventory[i] == 구렁텅이의군주)
 		{
 			//플레이어 이미지바꿈
 		}
 	}
-	
-	if (arrow==기본총알)
-	{
-		if (damage==1)
-		{
-			_bullet->render();
-		}
-		if (damage==3) //카드먹으면 공2올라감
-		{
-			//총알더큰걸로 이미지 출력
-		}
-	}
-	if (arrow==유황)
-	{
-		// 총알에 이미지 바꿔서 출력
-	}
-	
 
-//////// 테스트
-	for (int i = 0; i < vInventory.size(); i++)
-	{
-		wsprintf(buffer, TEXT("플레이어인벤:%d"), vInventory[i]);
-		TextOut(getMemDC(), 100, 10+20*i, buffer, lstrlen(buffer));
-	}
-	
-	wsprintf(buffer, TEXT("bomb:%d,  key:%d  money:%d"), bomb, key, money);
-	TextOut(getMemDC(), 10, 10, buffer, lstrlen(buffer));
-<<<<<<< HEAD
-	_bullet->render();
 	//RectangleMake(getMemDC(), rc);
 	if (KEYMANAGER->isStayKeyDown('D'))
 	{
@@ -270,8 +161,8 @@ void cPlayer::render(void)
 
 	switch (faceState)
 	{
-	case pLEFT: 
-		IMAGEMANAGER->render("아이작왼쪽얼굴", getMemDC(), rcHead.left, rcHead.top, 0,0, PLAYERHEADSIZEX, PLAYERHEADSIZEY);
+	case pLEFT:
+		IMAGEMANAGER->render("아이작왼쪽얼굴", getMemDC(), rcHead.left, rcHead.top, 0, 0, PLAYERHEADSIZEX, PLAYERHEADSIZEY);
 		cout << "left" << endl;
 		break;
 	case pRIGHT:
@@ -286,12 +177,23 @@ void cPlayer::render(void)
 	default:
 		break;
 	}
-	
+
+	//==================================================================
+	//		## 수치확인용 ##
+	//==================================================================
+	for (int i = 0; i < vInventory.size(); i++)
+	{
+		wsprintf(buffer, TEXT("플레이어인벤:%d"), vInventory[i]);
+		TextOut(getMemDC(), 100, 10 + 20 * i, buffer, lstrlen(buffer));
+	}
+
+	wsprintf(buffer, TEXT("bomb:%d,  key:%d  money:%d"), bomb, key, money);
+	TextOut(getMemDC(), 10, 10, buffer, lstrlen(buffer));
 }
 
 void cPlayer::animation(void)
 {
-	if (fire==true)
+	if (fire == true)
 	{
 		returnCount++;
 		if (returnCount > 50)
@@ -299,7 +201,7 @@ void cPlayer::animation(void)
 			fire = false;
 			returnCount = 0;
 			faceState = pIDLE;
-		
+
 		}
 	}
 	if (KEYMANAGER->isStayKeyDown('D'))
@@ -308,21 +210,21 @@ void cPlayer::animation(void)
 		frameCount++;
 		if (frameCount % 5 == 0)
 			leftFrame++;
-		if (leftFrame==10)
+		if (leftFrame == 10)
 			leftFrame = 0;
 	}
 	if (KEYMANAGER->isStayKeyDown('A'))
 	{
-	//	frameCount = 0;
+		//	frameCount = 0;
 		frameCount++;
 		if (frameCount % 5 == 0)
 			rightFrame++;
-		if (rightFrame==10)
+		if (rightFrame == 10)
 			rightFrame = 0;
 	}
 	if (KEYMANAGER->isStayKeyDown('W'))
 	{
-	//	frameCount = 0;
+		//	frameCount = 0;
 		frameCount++;
 		if (frameCount % 5 == 0)
 			upDownFrame++;
@@ -331,17 +233,17 @@ void cPlayer::animation(void)
 	}
 	if (KEYMANAGER->isStayKeyDown('S'))
 	{
-//		frameCount = 0;
+		//		frameCount = 0;
 		frameCount++;
 		if (frameCount % 5 == 0)
 			upDownFrame++;
 		if (upDownFrame == 10)
 			upDownFrame = 0;
 	}
-
-=======
-
 	RectangleMake(getMemDC(), rc);
->>>>>>> origin/master
 }
 
+void cPlayer::addInVentory(int _item)
+{
+	vInventory.push_back(_item);
+}
--- cPlayer.h
+++ cPlayer.h
@@ -5,40 +5,29 @@
 #define PLAYERSIZEY 15*3
 #define PLAYERHEADSIZEX 28*3
 #define PLAYERHEADSIZEY 25*3
+class cItemManager;
+
 enum PlayerFaceState
 {
 	pLEFT,
 	pRIGHT,
 	pIDLE,
-    pUP,
+	pUP,
 };
-class cItemManager;
-
-enum ESTATE
-{
-	IDLE,
-	DAMAGED,
-	// 카드애니매이션일떄 카드먹을때 아이템쪽에서 능력치올려주고 플레이어상태를 얘로바꿈 
-	// 플레이어쪽에서는 얘상태일때 약드는애니매이션? 출력하고 프레임이 최대로 가면 IDLE로 바꾸는로직짜면될듯
-	CARDANIM, 
-};
-
 
 class cPlayer : public gameNode
 {
 private:
-	TCHAR buffer[256];
+	TCHAR buffer[256]; // Textout할때 쓰는용도!
 
-	cItemManager* _im;
 	bullet* _bullet;
-	PlayerFaceState faceState; 
+	cItemManager* _im;
+	PlayerFaceState faceState;
 	PlayerFaceState keyState;
 	RECT rc;
 	RECT rcHead;
 	float x;
-	float y;	
-<<<<<<< HEAD
-	int currentmap;
+	float y;
 	//애니메이션 
 	int frameCount;
 	int returnCount;
@@ -46,19 +35,19 @@ private:
 	int leftFrame;
 	int rightFrame;
 	int upDownFrame;
-=======
-	int state = IDLE;
+
 
 	// 만약에 날개아이템(구렁텅잉의군주?)을 먹으면 아이템 매니져쪽에서 얘를 true로 바꿀거임.
 	// 맵매니져쪽에서 픽셀충돌을 IsFly==false일때만 검사하고 true가되면 픽셀충돌 하지않게 짜면될듯?
-	bool IsFly = false; 
+	bool IsFly = false;
+
 
 	vector<int> vInventory;
->>>>>>> origin/master
 
-//==================================================================
-//		## 스탯 ## 추가로들어갈거 수정필수!
-//==================================================================
+
+	//==================================================================
+	//		## 스탯 ## 추가로들어갈거 수정필수!
+	//==================================================================
 	int hp;  //체력
 	int range; //총알범위
 	int damage;	//대미지
@@ -66,9 +55,9 @@ private:
 	float moveSpeed; //이동속도
 	float bulletSpeed; //총알속도 얘는 바뀌는건지 아닌지 모르겟슴;
 
-//==================================================================
-//		## 아이템 ##  추가로들어갈거 수정필수!
-//==================================================================
+	//==================================================================
+	//		## 아이템 ##  추가로들어갈거 수정필수!
+	//==================================================================
 	int money;	//돈
 	int key;	//열쇠
 	int bomb;   //폭탄
@@ -78,7 +67,7 @@ private:
 
 public:
 	void SetIM(cItemManager* iim) { _im = iim; }
-	
+
 	bool GetIsFly() { return IsFly; }
 	void SetIsFly(bool _IsFly) { IsFly = _IsFly; }
 
@@ -86,7 +75,7 @@ public:
 	void SetMoney(int _money) { money = _money; }
 
 	int GetKey() { return key; }
-	void SetKey(int _key) { key = _key; }	
+	void SetKey(int _key) { key = _key; }
 
 	int GetBomb() { return bomb; }
 	void SetBomb(int _bomb) { bomb = _bomb; }
@@ -103,8 +92,10 @@ public:
 	float GetY() { return y; }
 	void SetY(float _y) { y = _y; }
 
+	void addInVentory(int _item); // 아이템 매니져에서 씀
+
 	RECT GetRC() { return rc; }
-	
+
 	bullet* getBullet() { return _bullet; }
 
 	HRESULT init(void);
@@ -113,6 +104,6 @@ public:
 	void render(void);
 	void animation(void);
 	cPlayer() {}
-	~cPlayer(){}
+	~cPlayer() {}
 };
 
